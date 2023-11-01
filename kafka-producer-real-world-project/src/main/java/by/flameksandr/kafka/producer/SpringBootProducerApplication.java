395e2c197fee17966bd8535bde8d8802faab6081
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-01 19:49:51 +0300

    Producer Implementation

--- kafka-producer-real-world-project/src/main/java/by/flameksandr/kafka/producer/SpringBootProducerApplication.java
+++ kafka-producer-real-world-project/src/main/java/by/flameksandr/kafka/producer/SpringBootProducerApplication.java
@@ -1,13 +1,23 @@
 package by.flameksandr.kafka.producer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
+
+    public SpringBootProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class, args);
     }
 
+    private WikimediaChangesProducer wikimediaChangesProducer;
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
+    }
 }
