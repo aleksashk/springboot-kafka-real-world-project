283f00d7b061366535ec2b51d2e03ae1ad814cdc
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-11-01 19:04:22 +0300

    create wikimedia producer

---
+++ kafka-producer-real-world-project/src/test/java/by/flameksandr/kafka/producer/KafkaProducerRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerRealWorldProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
